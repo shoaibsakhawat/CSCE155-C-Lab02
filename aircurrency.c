07798a053b1df89fabd79d29d3a4a54c489a8f9e
shoaib sakhawat <shoaibkarlal23@gmail.com>
2023-10-21 18:54:31 +0500

    make a variables

---
+++ aircurrency.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+     float britishPound = 0.79;
+     float japanniceYen = 127.65;
+
+    double dollar, new_jappanieseYen = 0,new_britishPound = 0,exchange_rate = 0,remaning_USD =0;
+
+    
+
+
+
+    return 0;
+}
